3023a90faf04135bc47fa87ae12ce3182a582e5a
Rajat Bhardwaj <rajatbhardwajg05@gmail.com>
2026-08-06 08:40:36 +0530

    Time: 10 ms (7.51%), Space: 9.1 MB (9.6%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        for(int i=0;i<31;i++){
+            if(pow(3,i)==n){
+                return true;
+            }
+        }
+        return false;
+    }
+};
